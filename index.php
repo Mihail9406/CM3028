f3a8d554cecb36e369507c0141e8386e86f1a981
Mihail9406 <mihail9406gmail.com>
2016-10-24 14:42:45 +0100

    trilolo

--- index.php
+++ index.php
@@ -3,18 +3,33 @@
 <html>
 
     <head>
-
+        <meta charset="UTF-8">
+        <title>Login Page</title>
     </head>
 
         <body>
-
+            <form action="checklogin.php" method="post">
+                Name:<br>
+                <input type="text" name="name">
+                    <br>
+                    Password:<br>
+                    <input type="password" name="password">
+                    <br><br>
+                <input type="submit" value="Submit">
+            </form>
             <p>
 
              <?php
 
-             echo "Hello World";
+            /* echo "Hello World";
              echo "<br>". "Hello" . " you fools" . " " ."Good luck with getting a pass" . "!";
-             echo "<br>". 5*10;
+             echo "<br>". 5*10; */
+
+             $username = "Mihail";
+             $password = "mypassword";
+
+             setcookie(`access_level`,`standardUser`);
+
              ?>
 
             </p>
