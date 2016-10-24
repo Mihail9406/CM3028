4b7308ce4b68ff43f6a509789e3e4b1efa1461cb
Mihail9406 <mihail9406gmail.com>
2016-10-24 15:46:50 +0100

    trilolo

--- checklogin.php
+++ checklogin.php
@@ -5,8 +5,8 @@
  * Date: 24/10/2016
  * Time: 15:40
  */
-$username = $_POST["Mihail"];
-$password = $_POST["mypassword"];
+$username = $_POST["name"];
+$password = $_POST["password"];
 
 if($username=="username"&&$password=="password"){
     setcookie('access_level','standardUser');
