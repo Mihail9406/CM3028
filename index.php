62ea22701cf781142f78ea111f77325b8cb9f7e5
Mihail9406 <mihail9406gmail.com>
2016-10-24 15:44:52 +0100

    trilolo

--- index.php
+++ index.php
@@ -19,14 +19,6 @@
             </form>
             <p>
 
-             <?php
-
-            /* echo "Hello World";
-             echo "<br>". "Hello" . " you fools" . " " ."Good luck with getting a pass" . "!";
-             echo "<br>". 5*10; */
-
-             ?>
-
             </p>
 
         </body>
