d9045797e889c8d5d9d570b6a1c0da256267985f
mihail9406 <mihail9406@gmail.com>
2016-11-02 17:39:43 +0200

    c5

---
+++ week3/sql_query.php
@@ -0,0 +1,14 @@
+<?php
+
+$sql_query = "SELECT * FROM superheroes WHERE superpower LIKE '%laser%'";
+$result = $db->query($sd_query);
+
+while($row = $result->fetch_array()){
+    echno"<p>".$row['superheroName'].'</p>';
+}
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2.11.2016 г.
+ * Time: 17:34
+ */
