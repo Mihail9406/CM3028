57d06040309868f3bb518ca542cdcaf574d5cc79
mihail9406 <mihail9406@gmail.com>
2016-11-02 17:08:34 +0200

    c3

---
+++ week3/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_SERVER','servername');
+define('DB_USERNAME','username');
+define('DB_PASSWORD','password');
+define('DB_DATABASE','database');
+
+$db = mysqli_connect(DB_SERVER,DB_SERVER,DB_PASSWORD,DB_DATABASE);
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2.11.2016 г.
+ * Time: 16:51
+ */
