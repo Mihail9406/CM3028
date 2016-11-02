bc0a519302d9961085e4cc6243a082d1f4178424
mihail9406 <mihail9406@gmail.com>
2016-11-02 18:28:05 +0200

    c6

--- week3/db_connect.php
+++ week3/db_connect.php
@@ -1,11 +1,23 @@
 <?php
 
-define('DB_SERVER','servername');
-define('DB_USERNAME','username');
-define('DB_PASSWORD','password');
-define('DB_DATABASE','database');
+define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','b18094a5a55464');
+define('DB_PASSWORD','1dead662');
+define('DB_DATABASE','d_database');
 
 $db = mysqli_connect(DB_SERVER,DB_SERVER,DB_PASSWORD,DB_DATABASE);
+
+$db = new mysqli(
+    'ap-cdbr-azure-east-c.cloudapp.net',
+    'b18094a5a55464',
+    '1dead662',
+    'd_database'
+);
+
+//test if a connection is established
+if($db->connect_errno){
+    die('Connect failed['.$db->connect_errno.']');
+}
 /**
  * Created by PhpStorm.
  * User: User
