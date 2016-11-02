2801d38b48ae8fa4402fb060d9c38b99125d1d3c
mihail9406 <mihail9406@gmail.com>
2016-11-02 18:34:42 +0200

    c6

--- week3/sql_query.php
+++ week3/sql_query.php
@@ -2,15 +2,9 @@
 
 include("db_connect.php");
 $sql_query = "SELECT * FROM marvelmovies";
-$result = $db->query($sd_query);
+$result = $db->query($sql_query);
 
 while($row = $result->fetch_array()){
     echo"<p>".$row['superheroName'].'</p>';
 }
 ?>
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 2.11.2016 г.
- * Time: 17:34
- */
